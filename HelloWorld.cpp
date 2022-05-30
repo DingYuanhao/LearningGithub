5e16c711cdc9421fd2fdaae3522f72ac4d59b962
DingYuanhao <dingyuanhao14@126.com>
2022-05-30 17:31:31 +0800

    Change HelloWorld CPP

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+int add(int a, int b) {
+    return a + b;
+}
+
+int multi(int a, int b) {
+    return a * b;
+}
+
 int main() {
-    cout<<"Hello World";
+    cout<<add(2, 5);
+    cout<<multi(2, 5);
     return 0;
 }
